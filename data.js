3fb3e9adf8adb669f668e1514eac52fea789fd18
cs-hubs <adityanimiwal3@gmail.com>
2026-08-02 01:11:48 +0530

    Add website data

---
+++ data.js
@@ -0,0 +1,158 @@
+/* ==========================================
+   CS INFORMATION
+   Website Data
+========================================== */
+
+const SETTINGS = {
+
+    whatsapp:
+    "https://whatsapp.com/channel/0029VbAtyUeDDmFQteHb3a33",
+
+    instagram:
+    "https://www.instagram.com/cs.information/",
+
+    browser:
+
+    "Microsoft Edge + AdGuard Extension"
+
+};
+
+/* ============================
+   Announcement
+============================ */
+
+const ANNOUNCEMENTS = [
+
+"⚠️ I am NOT the owner of any listed website.",
+
+"🛠️ If any website is temporarily down, please wait. It may be under maintenance.",
+
+"📚 If one website isn't working, try another available website.",
+
+"📱 Use Microsoft Edge + AdGuard Extension for the best experience."
+
+];
+
+/* ============================
+   PW Websites
+============================ */
+
+const PW = [
+
+{
+
+name:"PWTHOR",
+
+url:"https://pwthor.live/",
+
+status:"Open",
+
+recommended:false
+
+},
+
+{
+
+name:"StudyStark",
+
+url:"http://StudyStark.in",
+
+status:"Open",
+
+recommended:false
+
+},
+
+{
+
+name:"StudyRatna",
+
+url:"http://StudyRatna.cc",
+
+status:"Open",
+
+recommended:true
+
+},
+
+{
+
+name:"RareStudy",
+
+url:"https://rarestudy.site",
+
+status:"Key Generation",
+
+recommended:false
+
+},
+
+{
+
+name:"VedStudy",
+
+url:"http://vedstudy.com",
+
+status:"Key Generation",
+
+recommended:false
+
+},
+
+{
+
+name:"StudyRays",
+
+url:"http://StudyRays.cc",
+
+status:"Server 3",
+
+recommended:false
+
+}
+
+];
+
+/* ============================
+   Unacademy Websites
+============================ */
+
+const UNACADEMY = [
+
+{
+
+name:"StudyBeePro",
+
+url:"http://studybeepro.in",
+
+status:"Login",
+
+recommended:false
+
+},
+
+{
+
+name:"PrimeStudy",
+
+url:"http://Primestudy.site",
+
+status:"Login",
+
+recommended:false
+
+},
+
+{
+
+name:"StudyUK",
+
+url:"https://studyuk.online/offline",
+
+status:"Open",
+
+recommended:true
+
+}
+
+];
